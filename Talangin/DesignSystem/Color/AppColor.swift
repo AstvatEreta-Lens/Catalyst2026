5864db7e6892c1ee9f985fc6b9a40dcd495b03ac
Rifqi Rahman <rifqi.efforts@gmail.com>
2026-01-14 17:50:18 +0700

    chore: add color to design system

--- Talangin/DesignSystem/Color/AppColor.swift
+++ Talangin/DesignSystem/Color/AppColor.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 // Sesuaikan dengan yang ada di figma
 enum AppColors {
-
-    // Text
-    static let primaryText = ColorTokens.gray600
-    static let secondaryText = ColorTokens.gray300
-
-    // Background
-    static let primaryBackground = ColorTokens.white
-    static let secondaryBackground = ColorTokens.gray100
-
-    // Button
-    static let primaryButton = ColorTokens.primaryBlue
-    static let destructiveButton = ColorTokens.error
-
-    // Status
-    static let success = ColorTokens.success
-    static let warning = ColorTokens.warning
-    static let error = ColorTokens.error
-    
-    // custom
-    
-    
+//
+//    // Text
+//    static let primaryText = ColorTokens.gray600
+//    static let secondaryText = ColorTokens.gray300
+//
+//    // Background
+//    static let primaryBackground = ColorTokens.white
+//    static let secondaryBackground = ColorTokens.gray100
+//
+//    // Button
+//    static let primaryButton = ColorTokens.primaryBlue
+//    static let destructiveButton = ColorTokens.error
+//
+//    // Status
+//    static let success = ColorTokens.success
+//    static let warning = ColorTokens.warning
+//    static let error = ColorTokens.error
+//    
+//    // custom
+//    
+//    
     
 }
